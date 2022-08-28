39414b8270c5f707e1b980d63a81ec4ac14cc89a
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-08-28 18:31:24 -0300

    addStretch() function add space between widgets

--- pyqt6_book/pyqt6_book_4/pyqt6_book_4.py
+++ pyqt6_book/pyqt6_book_4/pyqt6_book_4.py
@@ -20,15 +20,18 @@ class JanelaPrincipal(QWidget):
         botao2 = QPushButton('Botão 2',self)
         botao3 = QPushButton('Botão 3',self)
 
+        layout.addStretch()
         layout.addWidget(botao1)
         layout.addWidget(botao2)
         layout.addWidget(botao3)
+        layout.addStretch()
         
         self.setLayout(layout)
 
         botao0 = QPushButton('SAIR', self)
         layout.addWidget(botao0)
-        botao0.move(275,260)
+        #layout.addStretch()
+        #botao0.move(275,260)
         botao0.clicked.connect(self.confirma_saida)
 
         self.show()
