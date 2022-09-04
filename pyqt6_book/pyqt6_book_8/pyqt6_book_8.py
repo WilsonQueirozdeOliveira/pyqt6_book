2342ee302353197fca15ec07c5b2db01b1c915d4
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-09-04 18:06:44 -0300

    call functions with tables

--- pyqt6_book/pyqt6_book_8/pyqt6_book_8.py
+++ pyqt6_book/pyqt6_book_8/pyqt6_book_8.py
@@ -10,25 +10,65 @@ class JanelaPrincipal(QWidget):
         super().__init__()
         self.setWindowTitle('Cadastro de Clientes')
         self.setWindowIcon(QIcon('icone.png'))
-        self.setGeometry(150,150,365,300)
+        self.setGeometry(150,150,500,400)
         self.Interface()
         
     def Interface(self):
-        layout = QVBoxLayout()
+        layout = QFormLayout()
         
         self.tabela = QTableWidget()
         self.tabela.setRowCount(10)
-        self.tabela.setColumnCount(5)
+        self.tabela.setColumnCount(4)
+        self.tabela.setHorizontalHeaderItem(0, QTableWidgetItem('Nome'))
+        self.tabela.setHorizontalHeaderItem(1, QTableWidgetItem('Idade'))
+        self.tabela.setHorizontalHeaderItem(2, QTableWidgetItem('Telefone'))
+        self.tabela.setHorizontalHeaderItem(3, QTableWidgetItem('E-mail'))
+
+        #self.tabela.horizontalHeader().hide()
+        #self.tabela.verticalHeader().hide()
+
+        self.tabela.setItem(0, 0, QTableWidgetItem('Fernado'))
+        self.tabela.setItem(0, 1, QTableWidgetItem('34'))
+        self.tabela.setItem(0, 2, QTableWidgetItem('55991357259'))
+        self.tabela.setItem(0, 3, QTableWidgetItem('fernado2rad@gmail.com'))
+        self.tabela.setItem(2, 0, QTableWidgetItem('Maria'))
+
+        #bloquear edição
+        #self.tabela.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+
+        self.tabela.doubleClicked.connect(self.instancia_elemento)
 
         botao0 = QPushButton('SAIR', self)
         #botao0.move(275,260)
         botao0.clicked.connect(self.confirma_saida)
+        botao1 = QPushButton('Salvar', self)
+        botao1.clicked.connect(self.salva_dados)
+
+        layout_buttons = QHBoxLayout()
+        layout_buttons.addStretch()
+        layout_buttons.addWidget(botao1)
+        layout_buttons.addWidget(botao0)
+        layout_buttons.addStretch()
 
         layout.addWidget(self.tabela)
-        layout.addWidget(botao0)
+        layout.addRow(layout_buttons)
 
         self.setLayout(layout)
         self.show()
+
+    def salva_dados(self):
+        base = []
+        for dado in self.tabela.selectedItems():
+            base.append(dado)
+
+            print(f'O elemento {dado.text()} está localizado na linha {dado.row()} e na coluna {dado.column()}')
+        pass
+
+    def instancia_elemento(self):
+        for dado in self.tabela.selectedItems():
+            print(f'O elemento {dado.text()} está localizado na linha {dado.row()} e na coluna {dado.column()}')
+
+        pass
 
     def confirma_saida(self):
         confirma = QMessageBox.question(self,
