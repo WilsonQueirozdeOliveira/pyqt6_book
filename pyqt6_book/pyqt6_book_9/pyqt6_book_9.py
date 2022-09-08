11ec399413f0e00e421b19bcb2c158d78d7ca867
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-09-07 22:11:41 -0300

    added three tabs

--- pyqt6_book/pyqt6_book_9/pyqt6_book_9.py
+++ pyqt6_book/pyqt6_book_9/pyqt6_book_9.py
@@ -14,11 +14,27 @@ class JanelaPrincipal(QWidget):
         self.Interface()
         
     def Interface(self):
+        layout = QVBoxLayout()
+
+        self.abas = QTabWidget()
+        
+        self.aba1 = QWidget()
+        self.abas.addTab(self.aba1, 'Principal')
+        self.aba2 = QWidget()
+        self.abas.addTab(self.aba2, 'inventário')
+        self.aba3 = QWidget()
+        self.abas.addTab(self.aba3, 'sobre')
+
+        layout.addWidget(self.abas)
+
 
         botao0 = QPushButton('SAIR', self)
-        botao0.move(275,260)
+        #botao0.move(275,260)
         botao0.clicked.connect(self.confirma_saida)
 
+        layout.addWidget(botao0)
+
+        self.setLayout(layout)
         self.show()
 
     def confirma_saida(self):
