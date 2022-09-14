a6ee46d0703550bd1134068cc3216a483daab1b7
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-09-13 22:25:02 -0300

    inputs layout update

--- tec_system/tec_system.py
+++ tec_system/tec_system.py
@@ -10,7 +10,7 @@ class JanelaPrincipal(QWidget):
         super().__init__()
         self.setWindowTitle('Cadastro de Clientes')
         self.setWindowIcon(QIcon('icone.png'))
-        self.setGeometry(0,30,680,600)
+        self.setGeometry(0,30,680,800)
         self.Interface()
         
     def Interface(self):
@@ -36,7 +36,7 @@ class JanelaPrincipal(QWidget):
         
         #frequency
         layout_frequencia = QHBoxLayout()
-        self.frequencia = QLabel('Frequencia(Hz):')
+        self.frequencia = QLabel('Frequência [Hz]:')
         #layout_input_column_0_tab2.addWidget(self.frequencia)
         layout_frequencia.addWidget(self.frequencia)
         self.input_frequencia = QLineEdit(self)
@@ -48,7 +48,7 @@ class JanelaPrincipal(QWidget):
         
         # Poles
         layout_polos = QHBoxLayout()
-        self.polos = QLabel('Polos(Nº):')
+        self.polos = QLabel('Polos [Nº]:')
         #layout_input_column_0_tab2.addWidget(self.polos)
         layout_polos.addWidget(self.polos)
         self.input_polos = QLineEdit(self)
@@ -59,7 +59,7 @@ class JanelaPrincipal(QWidget):
         layout_input_column_0_tab2.addLayout(layout_polos)
 
         # Pear fo poles
-        self.par_de_polos = QLabel('Par de Polos(Nº): ')
+        self.par_de_polos = QLabel('Par de Polos [Nº]: ')
         layout_input_column_0_tab2.addWidget(self.par_de_polos)
         
         self.rps = QLabel('RPS:')
@@ -71,7 +71,7 @@ class JanelaPrincipal(QWidget):
 
         #slip
         layout_escoregamento = QHBoxLayout()
-        self.escoregamento = QLabel('Escoregamento(%):')
+        self.escoregamento = QLabel('Escoregamento [%]:')
         #layout_input_column_0_tab2.addWidget(self.escoregamento)
         layout_escoregamento.addWidget(self.escoregamento)
         self.input_escoregamento = QLineEdit(self)
@@ -85,6 +85,20 @@ class JanelaPrincipal(QWidget):
         self.rpm_real = QLabel('RPM de Saida ~Real: ')
         layout_input_column_0_tab2.addWidget(self.rpm_real)
 
+        # Functions
+        '''
+        funcoes = QLabel('Funções:')
+        layout_input_column_0_tab2.addWidget(funcoes)
+        funcao_1 = QLabel('Par de polos = Polos / 2')
+        layout_input_column_0_tab2.addWidget(funcao_1)
+        funcao_2 = QLabel('RPS = Frequência / ( Par de polos )')
+        layout_input_column_0_tab2.addWidget(funcao_2)
+        funcao_3 = QLabel('RPM = RPS * 60[s]')
+        layout_input_column_0_tab2.addWidget(funcao_3)
+        funcao_4 = QLabel('RPM ~Real = RPM - Escoregamento [%]')
+        layout_input_column_0_tab2.addWidget(funcao_4)
+        '''
+
         layout_input_column_0_tab2.addStretch()
 
         layout_tab2.addLayout(layout_input_column_0_tab2)
@@ -92,12 +106,12 @@ class JanelaPrincipal(QWidget):
         # Imput column 1 tab2
         layout_input_column_1_tab2 = QVBoxLayout()
         
-        velocidade_do_motor = QLabel('teste:')
+        velocidade_do_motor = QLabel('coluna2:')
         layout_input_column_1_tab2.addWidget(velocidade_do_motor)
-        frequencia = QLabel('teste:')
+        frequencia = QLabel('coluna2:')
         layout_input_column_1_tab2.addWidget(frequencia)
         input_frequencia = QLineEdit(self)
-        input_frequencia.setPlaceholderText('teste')
+        input_frequencia.setPlaceholderText('coluna2')
         layout_input_column_1_tab2.addWidget(input_frequencia)
         layout_input_column_1_tab2.addStretch()
 
@@ -144,42 +158,45 @@ class JanelaPrincipal(QWidget):
         escorregamento =0
         rpm_real = 0
         
-        try:    
-            if isinstance(float(self.input_polos.text()),float):
-                input_polos = int(self.input_polos.text())
-                par_de_polos = input_polos/2
-                if input_polos == 2 or input_polos == 4 or input_polos == 8 or input_polos == 16:
-                    self.par_de_polos.setText(
-                        'Par de Polos(Nº):'+str(par_de_polos))
-                    print('calcula par de polos')
-                    self.polos.setText('Polos(Nº):')
-        except:
-            print('erro ao converter input_polos para float')
-            self.polos.setText('Polos(Nº): Erro não usar virgual, usar ponto : 0.0')
+        if self.input_polos.text():
+            try:    
+                if isinstance(float(self.input_polos.text()),float):
+                    input_polos = int(self.input_polos.text())
+                    par_de_polos = input_polos/2
+                    if input_polos == 2 or input_polos == 4 or input_polos == 8 or input_polos == 16:
+                        self.par_de_polos.setText(
+                            'Par de Polos [Nº]: '+str(par_de_polos))
+                        print('calcula par de polos')
+                        self.polos.setText('Polos [Nº]:')
+            except:
+                print('erro ao converter input_polos para float')
+                self.polos.setText('Polos [Nº]: Erro. Erro entrada float : 0.0')
 
-        try:
-            if isinstance(float(self.input_frequencia.text()),float) and par_de_polos:
-                frequencia = int(self.input_frequencia.text())
-                self.rps.setText('RPS: '+str(frequencia/par_de_polos))
-                rpm = (frequencia/par_de_polos)*60
-                self.rpm.setText('RPM: '+str(rpm)+' Sem escorregamento.')
-                print('calcula rpm')
-                self.frequencia.setText('Frequencia(Hz):')
-        except:
-            print('erro ao converter input_frequencia para float')
-            self.frequencia.setText('Frequencia(Hz): Erro não usar virgual, usar ponto : 0.0')
+        if self.input_frequencia.text():
+            try:
+                if isinstance(float(self.input_frequencia.text()),float) and par_de_polos:
+                    frequencia = int(self.input_frequencia.text())
+                    self.rps.setText('RPS: '+str(frequencia/par_de_polos))
+                    rpm = (frequencia/par_de_polos)*60
+                    self.rpm.setText('RPM: '+str(rpm)+' Sem escorregamento.')
+                    print('calcula rpm')
+                    self.frequencia.setText('Frequência [Hz]:')
+            except:
+                print('erro ao converter input_frequencia para float')
+                self.frequencia.setText('Frequência[Hz]: Erro. entrada float : 0.0')
 
-        try:
-            if isinstance(float(self.input_escoregamento.text()),float):
-                    print('calcula escorregamento')
-                    escorregamento = float(self.input_escoregamento.text())
-                    rpm_real = rpm-(((rpm/100)*escorregamento))
-                    self.rpm_real.setText('RPM de Saida ~Real: '+str(rpm_real))
-                    print('calcula rpm_real')
-                    self.escoregamento.setText('Escoregamento(%): ')
-        except:
-            print('erro ao converter input_escorregamento para float')
-            self.escoregamento.setText('Escoregamento(%): Erro não usar virgual, usar ponto : 0.0')
+        if self.input_escoregamento.text():
+            try:
+                if isinstance(float(self.input_escoregamento.text()),float):
+                        print('calcula escorregamento')
+                        escorregamento = float(self.input_escoregamento.text())
+                        rpm_real = rpm-(((rpm/100)*escorregamento))
+                        self.rpm_real.setText('RPM de Saida ~Real: '+str(rpm_real)+' [RPM]')
+                        print('calcula rpm_real')
+                        self.escoregamento.setText('Escoregamento [%]: ')
+            except:
+                print('erro ao converter input_escorregamento para float')
+                self.escoregamento.setText('Escoregamento [%]: Erro. entrada float : 0.0')
 
 
 
@@ -187,7 +204,7 @@ class JanelaPrincipal(QWidget):
         confirma = QMessageBox.question(self,
         #confirma = QMessageBox.critical(self,
                                         'Atenção',
-                                        'Deseja mesmo sair?',
+                                        'Dados não salvos serão perdidos, Deseja mesmo sair?',
                                         QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.Cancel)
         if confirma == QMessageBox.StandardButton.Yes:
             print('exit comfirmed')
@@ -199,8 +216,8 @@ class JanelaPrincipal(QWidget):
 
     def closeEvent(self, event):
         close = QMessageBox.question(self,
-                                         "QUIT",
-                                         "Are you sure want to stop the process?",
+                                         "Atenção",
+                                         "Dados não salvos serão perdidos, Deseja mesmo sair?",
                                          QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
         if close == QMessageBox.StandardButton.Yes:
             event.accept()
@@ -209,4 +226,5 @@ class JanelaPrincipal(QWidget):
       
 qt = QApplication(sys.argv)
 app = JanelaPrincipal()
+app.setStyleSheet('.QLabel { font-size: 13pt;} .QLineEdit { font-size: 13pt;}')
 sys.exit(qt.exec())
